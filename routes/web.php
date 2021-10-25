8e8ae32e1d9d49487dac14cebf4f8dec8d88335f
Sachin121kum <sk31817@gmail.com>
2021-10-25 12:03:37 +0530

    changes contact page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebsiteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,26 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 
-Route::get('/',[App\Http\Controllers\WebSiteController::class], 'index')->name('index');
-Route::get('category/{slug}',[App\Http\Controllers\WebSiteController::class], 'category')->name('category');
-Route::get('post/{slug}',[App\Http\Controllers\WebSiteController::class], 'post')->name('post');
-Route::get('page/{slug}',[App\Http\Controllers\WebSiteController::class], 'page')->name('page');
-Route::get('contact',[App\Http\Controllers\ShowController::class], 'contact')->name('contact.show');
-Route::post('contact',[App\Http\Controllers\ShowController::class], 'contact')->name('contact.submit');
+Route::get('/',[WebsiteController::class, 'index'])->name('index');
+Route::get('category/{slug}',[WebsiteController::class, 'category'])->name('category');
+Route::get('post/{slug}',[WebsiteController::class, 'post'])->name('post');
+Route::get('page/{slug}',[WebsiteController::class, 'page'])->name('page');
+Route::get('contact',[ShowController::class, 'contact'])->name('contact.show');
+Route::post('contact',[ShowController::class, 'contact'])->name('contact.submit');
 
 Route::group(['prefix' => 'admin', 'middleware' =>'auth'],function(){
-    Route::resources('categories',CategoryController);
-    Route::resource('posts',PostCOntroller);
-    Rout::resource('pages',PageController);
-    Route::resource('galleries',GalleryController);
+    // Route::resources('categories',CategoryController);
+    // Route::resource('posts',PostCOntroller);
+    // Rout::resource('pages',PageController);
+    // Route::resource('galleries',GalleryController);
 
 });
